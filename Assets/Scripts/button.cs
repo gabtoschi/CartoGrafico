11a434f206a473332f2881192a5fc801d7ba4a80
Luísa Moura <luisamoura@usp.br>
2018-07-19 17:15:09 -0300

    Initial map

---
+++ Assets/Scripts/button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class button : MonoBehaviour{
+    public GameObject player;
+
+    public void AbreQuestao(){
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<RectTransform>().position = EventSystem.current.currentSelectedGameObject.GetComponent<RectTransform>().position;
+    }
+}
