016826b64f843f4823a019bcf974403dc7f5b7be
danbarretto <daniel_barretto98@hotmail.com>
2018-07-23 17:04:44 -0300

    Add smooth Movement and Bug fixes

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MovementPlayer : MonoBehaviour{
+    public GameObject player;
+    public float tempoMov;
+    public GameObject buttons;
+     
+    public void AbreQuestao(){
+            StartCoroutine(MovePlayer(EventSystem.current.currentSelectedGameObject.GetComponent<RectTransform>()));
+    }
+
+    public IEnumerator MovePlayer(RectTransform finalPos)
+    {
+        Button[] btn = buttons.GetComponentsInChildren<Button>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        var currentPos = player.GetComponent<RectTransform>().position;
+        var t = 0f;
+        foreach(Button b in btn)
+        {
+            b.enabled = false;
+        }
+        while (t < 1)
+        {
+            t += Time.deltaTime / tempoMov;
+
+            player.GetComponent<RectTransform>().position = Vector3.Lerp(currentPos,finalPos.position , t);
+            yield return null;
+        }
+        foreach (Button b in btn)
+        {
+            b.enabled = true;
+        }
+
+    }
+
+}
