69669dbd193093d790e28fae91fabf3ef3c58bb1
Daniel Sá Barretto <daniel_barretto98@hotmail.com>
2018-08-12 13:02:45 -0300

    Level1 fixes and add victory screen

--- Assets/Scripts/PlayerEnter.cs
+++ Assets/Scripts/PlayerEnter.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerEnter : MonoBehaviour
 {
     public GameObject[] btnAdj;
     public GameObject player;
-    
+    public bool isFinal;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            for(int i=0; i<btnAdj.Length; i++) {
-                btnAdj[i].GetComponent<Image>().color = Color.yellow;
+            if (isFinal) {
+                //open victory pannel
+                SceneManager.LoadScene("TelaVitoria");
             }
-            player.GetComponent<MovementPlayer>().btnAdj = btnAdj;
-            Debug.Log(btnAdj.Length);
+            else {
+                for (int i = 0; i < btnAdj.Length; i++) {
+                    btnAdj[i].GetComponent<Image>().color = Color.yellow;
+                }
+                player.GetComponent<MovementPlayer>().btnAdj = btnAdj;
+                Debug.Log(btnAdj.Length);
+
+            }
         }
     }
     private void OnTriggerExit2D(Collider2D collision) {
@@ -26,5 +34,4 @@ public class PlayerEnter : MonoBehaviour
             }
         }
     }
-
 }
