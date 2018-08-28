316566087eb5c4a9ba55c3168b58648b092644c3
Daniel Sá Barretto <daniel_barretto98@hotmail.com>
2018-08-28 20:59:34 -0300

    Add Pause text for each level

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -10,7 +10,29 @@ public class Pause : MonoBehaviour {
     public Text textLevel;
 
     private void Start() {
-        
+        switch (PlayerPrefs.GetString("LevelSelected")) {
+            case "Salvador":
+                textLevel.text = "Nível 1\n Salvador - BA";
+                break;
+            case "Sao Paulo":
+                textLevel.text = "Nível 2\n São Paulo - SP";
+                break;
+            case "Curitiba":
+                textLevel.text = "Nível 3\n Curitiba - PR";
+                break;
+            case "Belem":
+                textLevel.text = "Nível 4\n Belém - PA";
+                break;
+            case "Brasilia":
+                textLevel.text = "Nível 5\n Brasília - DF";
+                break;
+            case "Rio de Janeiro":
+                textLevel.text = "Nível 6\n Rio de Janeiro - RJ";
+                break;
+            case "Rio Branco":
+                textLevel.text = "Nível 7\n Rio Branco - AC";
+                break;
+        }
     }
     public void OpenAndClosePause() {
         gameObject.SetActive(!gameObject.activeSelf);
