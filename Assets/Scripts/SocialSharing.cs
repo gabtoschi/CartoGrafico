ba328d103dd10198be02102d5fc554fd84859419
Gabriel Toschi <gabriel.toschi@hotmail.com>
2018-09-06 15:37:34 -0300

    whatsapp sharing button in end scene

---
+++ Assets/Scripts/SocialSharing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialSharing : MonoBehaviour {
+
+    private string whatsAppString = "Eu acabei de viajar pelo Brasil com o CartoGráfico! Viaje você também!";
+
+	private string whatsAppBaseURL = "https://wa.me/?text=";
+
+    public void shareWhatsApp(){
+		string wppURL = "";
+
+		wppURL += whatsAppBaseURL;
+		wppURL += whatsAppString;
+
+		Application.OpenURL(wppURL);
+	}
+}
