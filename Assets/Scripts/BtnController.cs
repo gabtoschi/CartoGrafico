d553b57b1d5ef96f639b30d753891abd6d929539
Daniel Sá Barretto <daniel_barretto98@hotmail.com>
2018-09-06 14:08:17 -0300

    Level and text changes

--- Assets/Scripts/BtnController.cs
+++ Assets/Scripts/BtnController.cs
@@ -42,7 +42,7 @@ public class BtnController : MonoBehaviour {
         }
         else
         {
-            SceneManager.LoadScene("menu");
+            SceneManager.LoadScene("LevelSelection");
         }
     }
 }
